dfe939772920045ccfe7dfa56bd34f9feea56f22
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-05 00:42:39 +0600

    built in routes

--- src/components/Counter/page.js
+++ src/components/Counter/page.js
@@ -10,8 +10,8 @@ const CounterComponent = () => {
     return (
         <div>
             <h1>{counter}</h1>
-            <button className='border bg-green-500 p-2' onClick={() => {setCounter(counter + 1)}}>Increase</button>
-            <button className='border bg-red-500 p-2' onClick={() => {setCounter(counter - 1)}}>Decrease</button>
+            <button className='btn btn-info' onClick={() => {setCounter(counter + 1)}}>Increase</button>
+            <button className='btn btn-info' onClick={() => {setCounter(counter - 1)}}>Decrease</button>
         </div>
     );
 };
