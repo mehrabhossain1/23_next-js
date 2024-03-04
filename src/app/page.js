187c075b89f947dbbee768b2c86f1af831c1b97c
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-04 21:36:08 +0600

    title name

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>This is next js HomePage</h1>
+      <h1 className='text-4xl text-center'>This is next js HomePage</h1>
     </div>
   );
 };
