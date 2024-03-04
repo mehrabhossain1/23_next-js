454cad47ef29a961679f940901a94ff1b4fee928
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-04 22:35:44 +0600

    basic routing

---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsHomePage = () => {
+    return (
+        <div>
+            <h1>This is news NewsHomePage</h1>
+        </div>
+    );
+};
+
+export default NewsHomePage;
