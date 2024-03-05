69a2bf78552b21ec3e674e9ed4ff427318cae758
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-05 15:17:38 +0600

    fixed a bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 import CounterComponent from "@/components/Counter/page";
 
-
 const HomePage = () => {
-
-
   return (
     <div>
-      <h1 className='text-4xl text-center'>This is next js HomePage</h1>
-      <CounterComponent/>
+      <h1 className="text-4xl text-center">This is next js HomePage</h1>
+      <CounterComponent />
     </div>
   );
 };
